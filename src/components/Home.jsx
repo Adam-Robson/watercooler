dbdae01cc1c74c29ce387669365c4b8b820365f7
adam robson <adamray312@gmail.com>
2023-03-29 08:57:08 -0700

    add context to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,21 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useUserContext } from './context/UserContext';
+
 import Todo from './Todo';
 
 export default function Home() {
   const [currentTodo, setCurrentTodo] = useState('');
   const [todoList, setTodoList] = useState([]);
+    
+  const { user } = useUserContext();
+
+  const navigate = useNavigate();
+
+  if (!user) {
+    navigate('/auth/sign-in');
+  }
+
   return (
     <>
       <section>
